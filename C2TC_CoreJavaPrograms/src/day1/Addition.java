738a24138462e3173aeeaf8de0c3b244b753ed03
HP <HP@DESKTOP-EMH3FN5>
2025-06-13 09:01:51 +0530

    I have performed addition program using Scanner class.

---
+++ C2TC_CoreJavaPrograms/src/day1/Addition.java
@@ -0,0 +1,15 @@
+package day1;
+
+
+public class Addition {
+
+	public static void main(String[] args) {
+	int a,b,c;
+	a=Integer.parseInt(args[0]);
+	b=Integer.parseInt(args[1]);
+	c=a+b;
+	System.out.println("Addition of "+a+" and "+b+" is " + c);
+
+	}
+
+}
