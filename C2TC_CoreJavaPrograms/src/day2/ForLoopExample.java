4cf2f6ff8fecd3264e0d1431589d8b657ec076ec
HP <HP@DESKTOP-EMH3FN5>
2025-06-15 18:55:14 +0530

    I have implemented the conditional and looping statements in java

---
+++ C2TC_CoreJavaPrograms/src/day2/ForLoopExample.java
@@ -0,0 +1,11 @@
+package day2;
+
+public class ForLoopExample {
+
+	public static void main(String[] args) {
+			for(int i=1;1<=1000; i++)
+			{
+				System.out.println("value of i:"+1);
+		}
+	}
+}
---
+++ C2TC_CoreJavaPrograms/src/day2/IfDemo.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		int a=5;
+		if(a!=5)
+			System.out.println("Value of x is not 5");
+		System.out.
+		println("Value of x is 5");
+	}
+
+}
