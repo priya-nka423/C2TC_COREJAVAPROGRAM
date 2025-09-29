b7e24114d4cd80af261efb7d62a0943206ec613e
HP <priyankaloganathan423@gmail.com>
2025-09-29 09:29:58 +0530

    This project contains my core java programs

---
+++ C2TC_CoreJavaPrograms/src/Day19/IStatement.java
@@ -0,0 +1,7 @@
+package Day19;
+
+@FunctionalInterface // interface is declared with only one abstract method
+interface IStatement {
+	public String show();// abstract Method
+	
+}
---
+++ C2TC_CoreJavaPrograms/src/Day19/IsOdd.java
@@ -0,0 +1,5 @@
+package Day19;
+@FunctionalInterface
+interface IsOdd{
+	public boolean checkOdd(int a);
+}
